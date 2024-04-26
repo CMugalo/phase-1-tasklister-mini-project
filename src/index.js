8572eab3676c0486df39e9c8a2412c04e1550050
Christian Mugalo <christian.mugalo@student.moringaschool.com>
2024-04-26 21:10:56 +0300

    adds event listeners

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let newInputId = document.getElementById("new-task-description");
+  newInputId.id = "taskDescription";
+
+  let form = document.getElementById("create-task-form");
+  form.addEventListener("submit", addTasks);
+
+  function addTasks(e) {
+    e.preventDefault();
+    let listItem = document.getElementById("tasks");
+    let listItemChild = document.createElement("li");
+    listItemChild.textContent = e.target.taskDescription.value;
+    listItem.appendChild(listItemChild);
+    form.reset();
+  }
 });
